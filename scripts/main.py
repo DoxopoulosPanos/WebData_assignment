453294864335f6f94a56705637c66c5974420759
panos <panosdoxos@gmail.com>
2020-02-01 22:29:11 +0100

    preprocessing: WARC_ID, tokenizer, remove_code_blocks, split_headers

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,18 @@
 import sys
 import logging
-import warc
+import gzip
+from bs4 import BeautifulSoup
 
 #  define logger as global variable
 logger = logging.getLogger(__name__)
 
+# define KEYNAME for records
+KEYNAME = "WARC-TREC-ID"
+
+
+def prerequisites():
+    import nltk
+    nltk.download('punkt')
 
 ########################################################
 #                      logger functions                #
@@ -25,34 +33,127 @@ def map_logging_level(level):
     return log_map[level]
 
 
-def set_logger(level="info"):
+def set_logger(stream_level="info", file_level="error", log_filename="file.log"):
     """
     This function is used in order to set the level of the global logger
-    :param level: logging level (e.g. logging.DEBUG)
+    :param stream_level: logging level for printing(e.g. logging.DEBUG)
+    :param file_level: logging level for writing in log file (e.g. logging.DEBUG)
+    :param log_filename: the path to the file that logs will be stored
     :return:
     """
+    global logger
+
     # find level
-    log_level = map_logging_level(level)
+    stream_log_level = map_logging_level(stream_level)
+    file_log_level = map_logging_level(file_level)
     # create console handler with a higher log level
-    handler = logging.StreamHandler()
-    file_handler = logging.FileHandler("file.log")
-    handler.setLevel(log_level)
-    file_handler.setLevel(log_level)
+    stream_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler(log_filename)
+    # set logger level
+    logger.setLevel(logging.DEBUG)
+    # set handler level
+    stream_handler.setLevel(stream_log_level)
+    file_handler.setLevel(file_log_level)
     # create formatter and add it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
+    stream_handler.setFormatter(formatter)
     file_handler.setFormatter(formatter)
     # add the handlers to the logger
-    logger.addHandler(handler)
+    logger.addHandler(stream_handler)
     logger.addHandler(file_handler)
 ########################################################
 ########################################################
 
 
+def find_labels(payload, labels):
+    key = None
+    for line in payload.splitlines():
+        if line.startswith(KEYNAME):
+            key = line.split(': ')[1]
+            break
+    for label, freebase_id in labels.items():
+        if key and (label in payload):
+            yield key, label, freebase_id
+
+
+def find_id(payload):
+    """
+    This function finds the WARC-TREC-ID
+    :param payload:
+    :return: a string with the WARC-TREC-ID
+    """
+    key = None
+    for line in payload.splitlines():
+        if line.startswith(KEYNAME):
+            key = line.split(': ')[1]
+            break
+    return key
+
+
+def split_records(stream):
+    payload = ''
+    for line in stream:
+        if line.strip() == "WARC/1.0":
+            yield payload
+            payload = ''
+        else:
+            payload += line
+
+
+def remove_code_blocks(text):
+    """
+    This function removes all blocs that starts with <!-- and ends at //-->
+    :param text: the text that will be processed
+    :return: a list without code blocks
+    """
+    textlines = text.splitlines()
+    new_lines = []
+    code_block = False
+    for line in textlines:
+        if line.startswith("<!--"):
+            code_block = True
+        elif line.startswith("//-->"):
+            code_block = False
+        elif code_block is False:
+            new_lines.append(line)
+
+    return new_lines
+
+
+def tokenizer(text_string):
+    """
+    This function uses nltk library and stanford tokenizer in order to split a string into tokens
+    :param text_string:
+    :return: a list with tokens
+    """
+    from nltk import word_tokenize
+    tokens = word_tokenize(text_string)
+    return tokens
+
+
+def split_headers(doc):
+    """
+    Returns only the headers and the body without the WARC and the connection variables/info
+    :param doc: string
+    :return: headers, body
+    """
+    if "Content-Type: text/html; charset=UTF-8" in doc:
+        headers, body = doc.split("Content-Type: text/html; charset=UTF-8")
+    else:
+        logger.warning("Missed document in split headers")
+        logger.debug("Document does not have Content-Type value")
+
+    return headers, body
+
+
+
 if __name__ == "__main__":
 
     # set logger
-    set_logger(level="info")
+    set_logger(stream_level="error", file_level="info", log_filename="file1.log")
+
+    #   nltk prerequisites
+    #prerequisites()
 
     # read input (filename)
     logger.debug("Read input (filename)")
@@ -62,15 +163,47 @@ if __name__ == "__main__":
         logger.error("Please provide a filename as input")
         raise IOError
 
-    records = warc.WARCFile(filename, "rb")
-    record_no = 2
-    for record in records:
-        if record_no > 0:
-            print record["Content-Type"]
-            print record.payload.read()
-            print "--------------------------"
-            print "--------------------------"
-        record_no -= 1
+    warcfile = gzip.open(sys.argv[1], "rt", errors="ignore")
+    record_no = 0
+    max_records = 4
+    for record in split_records(warcfile):
+        if record_no < max_records:
+            record_no += 1
+            logger.debug("record_no < {}".format(max_records))
+
+            logger.info("----------- Document No {}---------------".format(record_no))
+            if not record:      # if empty
+                logger.debug("EMPTY")
+                continue
+            if record_no == 2 or record_no == 3:
+                continue
+            soup = BeautifulSoup(record, "lxml")
+            logger.info(soup.text)
+            logger.info("==================================")
+            # split headers from body
+            headers, body = split_headers(soup.text)
+            # # HEADERS preprocessing
+            warc_id = find_id(headers)
+            if not warc_id:  # if empty
+                logger.debug("No ID. This file will be skipped")
+                continue
+            logger.info("ID: {}".format(warc_id))
+            # # BODY preprocessing
+            # remove code blocks
+            lines = remove_code_blocks(body)
+            # join all lines together
+            body = " ".join(lines)
+            # tokenize
+            tokens = tokenizer(body)
+            for token in tokens:
+                logger.info(token)
+            logger.info("--------------------------")
+            logger.info("--------------------------")
+        else:
+            logger.debug("record_no = {}".format(max_records))
+            logger.debug("Exiting...")
+            exit(0)
+
 
 
 
