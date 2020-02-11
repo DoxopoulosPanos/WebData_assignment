4fc342ffd40da377eeec4b275d01f185302471cb
panos <panosdoxos@gmail.com>
2020-02-11 11:35:58 +0100

    run example.py on DAS-4

--- scripts/elasticsearch.py
+++ scripts/elasticsearch.py
@@ -1,16 +1,16 @@
 import requests
 
 
-def search(domain, query):
+def search(domain, query, size=20):
     url = 'http://%s/freebase/label/_search' % domain
-    response = requests.get(url, params={'q': query, 'size': 1000})
+    response = requests.get(url, params={'q': query, 'size': size})
     id_labels = {}
     if response:
         response = response.json()
         for hit in response.get('hits', {}).get('hits', []):
             freebase_label = hit.get('_source', {}).get('label')
             freebase_id = hit.get('_source', {}).get('resource')
-            id_labels.setdefault(freebase_id, set()).add( freebase_label )
+            id_labels.setdefault(freebase_id, set()).add(freebase_label)
     return id_labels
 
 
