8a39ac278b32b0060965ecebd98e4dd1cc20e14a
panos <panosdoxos@gmail.com>
2020-02-12 16:57:45 +0100

    fix starter.sh

--- scripts/starter.sh
+++ scripts/starter.sh
@@ -1,10 +1,17 @@
 # allow prun command to be executed
 module load prun
 
-# starting Elastic search
+# Elastic search
 ES_PORT=9200
 ES_BIN=/var/scratch/wdps1934/wdps/elasticsearch-2.4.1/bin/elasticsearch
 
+# Trident
+KB_PORT=9090
+KB_BIN=/home/jurbani/trident/build/trident
+KB_PATH=/home/jurbani/data/motherkb-trident
+
+
+# starting Elastic search
 >.es_log*
 prun -o .es_log -v -np 1 ESPORT=$ES_PORT $ES_BIN </dev/null 2> .es_node &
 echo "waiting for elasticsearch to set up..."
@@ -13,12 +20,25 @@ ES_PID=$!
 until [ -n "$(cat .es_log* | grep YELLOW)" ]; do sleep 1; done
 echo "elasticsearch should be running now on node $ES_NODE:$ES_PORT (connected to process $ES_PID)"
 
-sleep 10
+sleep 5
+#####################################
+
+# starting Trident
+echo "Lauching an instance of the Trident server on a random node in the cluster ..."
+prun -o .kb_log -v -np 1 $KB_BIN server -i $KB_PATH --port $KB_PORT </dev/null 2> .kb_node &
+echo "Waiting 5 seconds for trident to set up (use 'preserve -llist' to see if the node has been allocated)"
+until [ -n "$KB_NODE" ]; do KB_NODE=$(cat .kb_node | grep '^:' | grep -oP '(node...)'); done
+
+sleep 5
+KB_PID=$!
+echo "Trident should be running now on node $KB_NODE:$KB_PORT (connected to process $KB_PID)"
+#####################################
 
 #python elasticsearch.py $ES_NODE:$ES_PORT "Vrije Universiteit Amsterdam"
 
 #prun -v -np 1 python preprocessing.py "/var/scratch/wdps1934/wdps/data/sample.warc.gz"
-prun -v -np 1 python example.py $ES_NODE:$ES_PORT "Vrije Universiteit Amsterdam"
+prun -v -np 1 python example.py $ES_NODE:$ES_PORT $KB_NODE:$KB_PORT "/var/scratch/wdps1934/wdps/data/sample.warc.gz"
+#prun -v -np 1 python example.py $ES_NODE:$ES_PORT "Vrije Universiteit Amsterdam"
 
 
 # kill elastic search server
