b9cbf1c0cac740a4908f6af2acc82de97507d1e7
panos <panosdoxos@gmail.com>
2020-02-19 21:53:45 +0100

    add scapy nlp functions

--- scripts/depedencies.sh
+++ scripts/depedencies.sh
@@ -4,3 +4,9 @@ pip install --user BeautifulSoup4
 pip install --user --upgrade setuptools
 pip install --user -U nltk
 pip install --user textdistance
+
+
+#-----------------------
+pip2 install -U spacy
+python2 -m spacy download en_core_web_sm
+pip2 install -U lxml
